e50d88d84285a6ffb46d505326bae9f6df70b4da
ANIMESH MANDAL <animeshmandal4400@gmail.com>
2022-10-22 18:15:31 +0530

    commit changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import { Route, BrowserRouter, Routes } from "react-router-dom";
 import { Home } from "../src/components/scripts/Home";
-import { Jobs } from "../src/components/scripts/Jobs";
-import { Messaging } from "../src/components/scripts/Messaging";
-import { MyNetwork } from "../src/components/scripts/MyNetwork";
 import { Navbar } from "../src/components/scripts/Navbar";
-import { Notification } from "../src/components/scripts/Notification";
 
 export const App = () => {
   return (
@@ -13,10 +9,6 @@ export const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/mynetwork" element={<MyNetwork />} />
-        <Route path="/jobs" element={<Jobs />} />
-        <Route path="/messaging" element={<Messaging />} />
-        <Route path="/notification" element={<Notification />} />
       </Routes>
     
       </BrowserRouter>
